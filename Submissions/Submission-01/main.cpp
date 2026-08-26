4aa419e6fe97fdc2f22d3315a04279ac1b94640b
Kristoffer Solum <kristofso@uia.no>
2026-08-26 12:03:51 +0200

    Add Step 1 complete

--- Submissions/Submission-01/main.cpp
+++ Submissions/Submission-01/main.cpp
@@ -3,7 +3,8 @@
 
 int main() {
 
-    //  Step 1a Test it with an int, a double, and a custom struct.
+    //  Step 1.1 a
+    //  Test it with an int, a double, and a custom struct.
     constexpr int test1 = 5;
     constexpr double test2 = 1.5;
     struct Tstruct1 {
@@ -12,17 +13,19 @@ int main() {
 
     };
     const Tstruct1 test3{5, "Hello world!"};
-    std::cout << "------------- Step 1a -------------" << std::endl;
+    std::cout << "------------- Step 1.1 a -------------" << std::endl;
     PrintTypeSize(test1);
     PrintTypeSize(test2);
     PrintTypeSize(test3);
 
-    //  Step 1b Test it with two int
-    std::cout << "------------- Step 1b -------------" << std::endl;
+    //  Step 1.1 b
+    //  Test it with two int
+    std::cout << "------------- Step 1.1 b -------------" << std::endl;
     int a=5, b=4;
     cout << GetMax(a, b) << endl;
 
-    //  Step 1c You must successfully pass a custom struct into GetMax
+    //  Step 1.1 c
+    //  You must successfully pass a custom struct into GetMax
     //  (e.g., a TStudent struct with a grade integer).
     //  For the template to compile, you will need to
     //  overload the > operator inside your custom struct!
@@ -37,9 +40,30 @@ int main() {
     TStudent student1 = {"Kristoffer", 4};
     TStudent student2 = {"Henrik", 5};
 
-    TStudent max = GetMax(student1, student2);
+    TStudent bestStudent = GetMax(student1, student2);
 
-    std::cout << "------------- Step 1b -------------" << std::endl;
-    cout << max.name << ": " << max.grade << endl;
+    std::cout << "------------- Step 1.1 c -------------" << std::endl;
+    cout << bestStudent.name << ": " << bestStudent.grade << endl;
+
+    // Step 1.2 a
+    TDataPack<int, double, std::string> dataPack = {5, 2.3, "hello"};
+    cout<<"------------- Step 1.2 a -------------"<<endl;
+    cout << dataPack.value << endl;
+    cout << dataPack.rest.value << endl;
+    cout << dataPack.rest.rest.value << endl;
+
+    // Step 1.2 b
+    TStaticArray<int, 3> numbers;
+    numbers.Populate(0, 1);
+    numbers.Populate(1, 2);
+    numbers.Populate(2, 3);
+
+    cout<<"------------- Step 1.2 b -------------"<<endl;
+    numbers.Print();
+
+    // Step 1.2 c
+    cout<<"------------- Step 1.2 c -------------"<<endl;
+    PrintContainer(numbers);
+
     return 0;
 }
--- Submissions/Submission-01/main.h
+++ Submissions/Submission-01/main.h
@@ -8,14 +8,16 @@
 #include <iostream>
 using namespace std;
 
-//  Step 1a Create a generic template function that accepts any data type
+//  Step 1.1 a
+//  Create a generic template function that accepts any data type
 //  and prints its size in bytes to the console (hint: use sizeof()).
 template <typename T>
 void PrintTypeSize(T v) {
     cout << sizeof(v) << endl;
 }
 
-// Step 1b Create a generic template function that returns the larger of two variables.
+// Step 1.1 b
+// Create a generic template function that returns the larger of two variables.
 template <typename T>
 T GetMax(T a, T b) {
     if (a > b) {
@@ -23,4 +25,56 @@ T GetMax(T a, T b) {
     }
     return b;
 }
+
+// Step 1.2 a
+// Variadic Template Struct:
+// Create a struct called TDataPack that can hold an arbitrary number of different data types
+// (e.g., template <typename... Args>). This acts like a custom Tuple.
+template <typename... Args>
+struct TDataPack;
+
+template <>
+struct TDataPack<>{
+};
+
+template <typename First, typename ...Rest>
+struct TDataPack<First, Rest...> {
+    First value;
+    TDataPack<Rest...> rest;
+};
+
+// Step 1.2 b
+// Create a TStaticArray class where both the data type and the array size are known at compile time.
+// This requires a non-type template parameter (e.g., template <typename T, int Size>).
+// Implement a method to populate it and a method to print its contents.
+template <typename T, int Size>
+class TStaticArray {
+private:
+    T data[Size]{};
+public:
+    void Populate(int index, T value) {
+        data[index] = value;
+    }
+    void Print() {
+        for (int i = 0; i < Size; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+// Step 1.2 c
+//  Create a function that accepts another template as its parameter.
+//  For example, create a PrintContainer function that specifically accepts yourTStaticArray blueprint.
+//  (Hint: the syntax looks like template <template <typename, int> class Container, typename T, int N>).
+
+template <
+    template <typename, int> class Container,
+    typename T,
+    int N
+>
+void PrintContainer(Container<T, N>& container) {
+    container.Print();
+}
+
 #endif //MAIN_H
